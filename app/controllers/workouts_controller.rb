31758fb647b9d550885efb1569e1921d10dea120
Konrad Pason <konrad.pason17@gmail.com>
2016-03-08 17:00:36 +0100

    Added some controllers

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,6 +1,10 @@
 class WorkoutsController < ApplicationController
-
+	before_action :find_workout, only: [:show, :edit, :update, :destroy]
 def index
+	@workouts = Workout.all.order("created_at DESC")
+end
+
+def show
 end
 
 def new
@@ -9,11 +13,37 @@ end
 
 def create
 	@workout = Workout.new(workout_params)
+
+	if @workout.save
+		redirect_to root_path
+	else
+		render 'new'
+	end
+end
+
+def edit
+end
+
+def update
+	if @workout.update(workout_params)
+		redirect_to workout_path(@workout)
+	else
+		render 'edit'
+	end
+	end
+
+def destroy
+	@workout.destroy
+	redirect_to root_path
 end
 
 private
 
 def workout_params
 	params.require(:workout).permit(:title, :description, :author)
+end
+
+def find_workout
+	@workout = Workout.find(params[:id])
 end
 end
